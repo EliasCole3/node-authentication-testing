acb09ddadfb1de7c71290f698c908f4a560b0e2d
Elias C <eliascole3@gmail.com>
2015-12-05 11:17:24 -0800

    updates

--- js/js-es6.js
+++ js/js-es6.js
@@ -354,11 +354,12 @@ let abc = {
         <option value='andora.jpg'>Andora</option>
         <option value='brement.jpg'>Brement</option>
         <option value='dark-forest-1.jpg'>Dark Forest</option>
+        <option value='desert-1.JPG'>Desert Statue</option>
         <option value='desert-statue.jpg'>Desert Statue</option>
         <option value='dunkar.jpg'>Dundar</option>
         <option value='forest-path-1.jpg'>Forest Path 1</option>
         <option value='forest-path-2.jpg'>Forest Path 2</option>
-        <option value='forest-1.jpg'>Forest 1</option>
+        <option value='forest-1.JPG'>Forest 1</option>
         <option value='plains-1.jpg'>Plains 1</option>
         <option value='plains-2.jpg'>Plains 2</option>
       </select>
