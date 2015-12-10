cb7865c8c3f9a4656412c0fbca60e1fea818bca2
Elias C <eliascole3@gmail.com>
2015-12-10 07:10:10 -0800

    updates

--- server.js
+++ server.js
@@ -45,11 +45,11 @@ app.use(bodyParser()); // get information from html forms
 // app.use(bodyParser.json())
 app.set('view engine', 'ejs'); // set up ejs for templating
 
-app.use(express.static('css'));
-app.use(express.static('fonts'));
-app.use(express.static('images'));
-app.use(express.static('js'));
-app.use(express.static('sounds'));
+// app.use(express.static('css'));
+// app.use(express.static('fonts'));
+// app.use(express.static('images'));
+// app.use(express.static('js'));
+// app.use(express.static('sounds'));
 app.use(express.static('static'));
 
 
